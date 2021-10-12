faf3952d10b5995b8eb37138dde5a45e8d8f1e47
admin <donchenkodenis2000@gmail.com>
2021-10-12 22:48:22 +0300

    edit task

--- hw_1_base_operations/src/main/java/ua/com/alevel/sumNumbers/SumNumbersFromString.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/sumNumbers/SumNumbersFromString.java
@@ -2,7 +2,6 @@ package ua.com.alevel.sumNumbers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
