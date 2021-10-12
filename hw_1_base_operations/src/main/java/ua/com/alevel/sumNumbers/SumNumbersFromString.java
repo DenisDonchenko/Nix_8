9c6eb1b122ea5d2e4bc4554066e4af968cebe9ef
admin <donchenkodenis2000@gmail.com>
2021-10-12 22:25:39 +0300

    edit first task

--- hw_1_base_operations/src/main/java/ua/com/alevel/sumNumbers/SumNumbersFromString.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/sumNumbers/SumNumbersFromString.java
@@ -15,7 +15,8 @@ public class SumNumbersFromString {
 
             System.out.println("This task takes a string from the console and extracts all the numbers and finds their sum.\n" +
                     "The program can accept floating point numbers and negative numbers.\n" +
-                    "Example (5, 54,-43,-3, 0.7, .43) ");
+                    "Example (5, 54,-43,-3, 0.7, .43) \n" +
+                    "Enter your string : ");
             inputString = reader.readLine();
             if (inputString.isEmpty()) {
                 throw new Exception();
